c2c81f13b8ba4f45e8c8104db77ee6636796fa11
noshi6669 <souta.yurino@icloud.com>
2021-05-20 11:39:16 +0900

    5/20　授業内容の追加

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,20 +1,43 @@
 #include <iostream>
 #include <DxLib.h>
+#include <sstream>
+#include <iomanip>
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
-
+	
 	ChangeWindowMode(true);
 	DxLib_Init();
 	SetDrawScreen(DX_SCREEN_BACK);
+
+	int graphH[6] = {};
+	//読み込みの時点でエラー出てる
+	for (int i = 0; i < 6; i++)
+	{
+		//wchar_t path[80];
+		std::wostringstream oss;
+		oss << L"./Asset/Adventurer-1.5/Individual Sprites/adventurer-run-%02d";
+		oss << std::setw(2);
+		oss << std::setfill(L'0');
+		oss << i << ".png";
+
+		
+		//wsprintf(path, L"../../Asset/Adventurer-1.5/ndividual Sprites/adventurer-run-%02d.png",80);
+		graphH[i] = LoadGraph(oss.str().c_str());
+	}
+	
+
+	unsigned int frameNo = 0;
 	while (ProcessMessage() != -1)
 	{
 		ClearDrawScreen();
 
 		int num = 0;
 		DrawBox(200, 200, 400, 400, 0x00ff00, true);
+		DrawRotaGraph(200, 200, 4.0f, 0.0f, graphH[frameNo / 10], true);
 
 		ScreenFlip();
+		frameNo = (frameNo + 1) % 60;
 	}
 
 	DxLib_End();
