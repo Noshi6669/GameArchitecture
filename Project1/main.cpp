44e386c75649d1d228b3554f6c1833136134e389
noshi6669 <souta.yurino@icloud.com>
2021-05-20 10:39:20 +0900

    test

---
+++ Project1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <DxLib.h>
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+
+	ChangeWindowMode(true);
+	DxLib_Init();
+	SetDrawScreen(DX_SCREEN_BACK);
+	while (ProcessMessage() != -1)
+	{
+		ClearDrawScreen();
+
+		int num = 0;
+		DrawBox(200, 200, 400, 400, 0x00ff00, true);
+
+		ScreenFlip();
+	}
+
+	DxLib_End();
+	return 0;
+}
