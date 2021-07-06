81f936671318e3e38cadfb9cee35b77d53d40648
noshi6669 <souta.yurino@icloud.com>
2021-07-06 11:16:58 +0900

    7/6

---
+++ shootemup/shootemup/Geometry.cpp
@@ -0,0 +1,112 @@
+#include <cmath>
+#include <utility>
+#include "Geometry.h"
+
+float Rect::Left() const
+{
+    return center.x - w;
+}
+
+float Rect::Right() const
+{
+    return center.x + w;
+}
+
+float Rect::Top() const
+{
+    return center.y - h;
+}
+
+float Rect::Bottom() const
+{
+    return center.y + h;
+}
+
+float Rect::Width() const
+{
+    return w * 2.0f;
+}
+
+float Rect::Height() const
+{
+    return h * 2.0f;
+}
+
+float Vector2::Magnitude() const
+{
+    return std::hypotf(x, y);
+}
+
+void Vector2::Normalize()
+{
+    auto mag = Magnitude();
+    x /= mag;
+    y /= mag;
+}
+
+Vector2 Vector2::Normalized() const
+{
+    auto mag = Magnitude();
+
+    return Vector2(x / mag, y / mag);
+}
+
+void Vector2::operator+=(const Vector2& v)
+{
+    x += v.x;
+    y += v.y;
+}
+
+void Vector2::operator-=(const Vector2& v)
+{
+    x -= v.x;
+    y -= v.y;
+}
+
+bool Vector2::operator==(const Vector2& v)
+{
+    return ((x == v.x) && (y == v.y));
+}
+
+void Vector2::Rotate90(void)
+{
+    std::swap(x, y);
+    x = -x;
+}
+
+Vector2 Vector2::Rotated90(void)
+{
+    return { -y,x };
+}
+
+
+//ベクトルの足し算
+Vector2 operator+(const Vector2& lval, const Vector2& rval)
+{
+    return Vector2{ lval.x + rval.x,lval.y + rval.y };
+}
+
+//ベクトルの引き算
+Vector2 operator-(const Vector2& lval, const Vector2& rval)
+{
+    return Vector2{ lval.x - rval.x,lval.y - rval.y };
+}
+
+//ベクトルのスカラー倍
+Vector2 operator*(const Vector2& lval, float num)
+{
+    return Vector2{ lval.x * num,lval.y * num };
+}
+
+//ベクトルのスカラー除
+Vector2 operator/(const Vector2& lval, float num)
+{
+    if (num != 0) 
+    {
+        return Vector2{ lval.x / num,lval.y / num };
+    }
+    else {
+        return Vector2{ 0,0 };
+    }
+
+}
---
+++ shootemup/shootemup/Geometry.h
@@ -0,0 +1,64 @@
+#pragma once
+
+//2Dベクトル
+struct Vector2
+{
+	float x, y;
+	Vector2() :x(0), y(0) {};
+	Vector2(float inx, float iny) :x(inx), y(iny) {};
+	//ベクトルの大きさを返す関数
+	float Magnitude()const;
+	//ベクトルを正規化する関数
+	void Normalize();
+	//ベクトルを正規化したベクトルを返す
+	Vector2 Normalized() const;
+
+
+	void operator+=(const Vector2& v);
+	void operator-=(const Vector2& v);
+	bool operator==(const Vector2& v);
+	//90度回転させる
+	void Rotate90(void);
+	Vector2 Rotated90(void);
+	static const Vector2 Zero() {
+		return { 0.0f,0.0f };
+	}
+};
+
+//ベクトルの足し算
+Vector2 operator+(const Vector2& lval, const Vector2& rval);
+
+//ベクトルの引き算
+Vector2 operator-(const Vector2& lval, const Vector2& rval);
+
+//ベクトルのスカラー倍
+Vector2 operator*(const Vector2& lval, float num);
+
+//ベクトルのスカラー除
+Vector2 operator/(const Vector2& lval, float num);
+
+//内積,外積
+float Dot(const Vector2&);
+
+//座標＝ベクトルなので、座標として定義する
+using Position2 = Vector2;
+
+//矩形の当たり判定
+struct Rect
+{
+	Position2 center;	//中心点
+	float w, h;			//w・・・幅,	h・・・高さ
+	float Left()const;		//左のＸ座標を返す
+	float Right()const;		//右のＸ座標を返す
+	float Top()const;		//
+	float Bottom()const;	//
+	float Width()const;
+	float Height()const;
+};
+
+//円の構造体
+struct Circle
+{
+	Position2 center;	//中心点
+	float radius;		//半径
+};
